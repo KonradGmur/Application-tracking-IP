5274cfcd6cd1ad1d0dc5642ad2f304b37ee05b69
Konrad <konrad.gmur@gmail.com>
2021-07-18 17:50:41 +0200

    Adding an image at the top of the page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
-	<div class="home"></div>
+	<div class="flex flex-col h-screen max-h-screen">
+		<!--Search / Results-->
+		<div
+			class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
+		></div>
+	</div>
 </template>
 
 <script>
