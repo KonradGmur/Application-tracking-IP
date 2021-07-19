bdd00d64b6ce20494e1443ae0e058ecc01c1c821
Konrad <konrad.gmur@gmail.com>
2021-07-19 18:52:30 +0200

    Add a header and Input at the top of the page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,16 @@
 		<div
 			class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
 		></div>
+		<!--Search Input-->
+		<div class="w-full max-w-screen-sm">
+			<h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+			<div class="flex">
+				<input
+					class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md"
+					type="text"
+				/>
+			</div>
+		</div>
 	</div>
 </template>
 
