80bbfd1f02221534937f249ac5ffa07d3f887131
Konrad <konrad.gmur@gmail.com>
2021-07-19 19:55:07 +0200

    Adding a component with a map - MAPBOX

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,31 +2,58 @@
 	<div class="flex flex-col h-screen max-h-screen">
 		<!--Search / Results-->
 		<div
-			class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
-		></div>
-		<!--Search Input-->
-		<div class="w-full max-w-screen-sm">
-			<h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
-			<div class="flex">
-				<input
-					class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
-					type="text"
-					placeholder="Search for any OP address or leave empty to get your ip info"
-				/>
-				<i
-					class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fas fa-chevron-right"
-				></i>
+			class="z-20 flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
+		>
+			<div class="w-full max-w-screen-sm">
+				<h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+				<!--Search Input-->
+				<div class="flex">
+					<input
+						class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+						type="text"
+						placeholder="Search for any OP address or leave empty to get your ip info"
+					/>
+					<i
+						class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fas fa-chevron-right"
+					></i>
+				</div>
+				<!--IP Info-->
+				<IPInfo />
 			</div>
-			<!--IP Info-->
-			<IPInfo />
 		</div>
+		<!--Map-->
+		<div id="mapid" class="h-full z-10"></div>
 	</div>
 </template>
 
 <script>
 import IPInfo from '../components/IPInfo.vue';
+import leaflet from 'leaflet';
+import { onMounted } from 'vue';
 export default {
 	name: 'Home',
 	components: { IPInfo },
+	setup() {
+		let mymap;
+		onMounted(() => {
+			mymap = leaflet.map('mapid').setView([51.505, -0.09], 13);
+
+			leaflet
+				.tileLayer(
+					'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29ucmFkZyIsImEiOiJja3Jhd3Bqcmkwc2Y0MzJxdTZhdTZjMWt6In0.OLzSV_Np8hw0JQhkr9lfKA',
+					{
+						attribution:
+							'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+						maxZoom: 18,
+						id: 'mapbox/streets-v11',
+						tileSize: 512,
+						zoomOffset: -1,
+						accessToken:
+							'pk.eyJ1Ijoia29ucmFkZyIsImEiOiJja3Jhd3Bqcmkwc2Y0MzJxdTZhdTZjMWt6In0.OLzSV_Np8hw0JQhkr9lfKA',
+					}
+				)
+				.addTo(mymap);
+		});
+	},
 };
 </script>
